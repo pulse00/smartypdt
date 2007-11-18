b03e5ac3a08c7143157b289cfe2ca896c4c6f0aa
ganoro <ganoro@908ba3c2-b53d-0410-a727-33fbc2eb2a91>
2007-11-18 20:34:03 +0000

    1. adding a missing jar 2. providing an extracted smarty compiler

--- org.eclipse.php.smarty.core/Ressources/SmartyCompiler.php
+++ org.eclipse.php.smarty.core/Ressources/SmartyCompiler.php
@@ -6,6 +6,12 @@ require_once 'Smarty-2.6.18/libs/Smarty_Compiler.class.php';
 
 $template = file_get_contents($_SERVER['argv'][1]);
 $smarty_compiler = new Smarty_Compiler();
+
+/**
+ * Add here your specific configuration to Smarty compiler
+ * $smarty_compiler->left_delimiter = '{--';
+ */
+ 
 $smarty_compiler->_compile_file('foo', $template, $compiled);
 
 exit(1);
